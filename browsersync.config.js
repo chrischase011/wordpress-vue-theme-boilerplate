f1e0c09de2a2f879048b6b1af1d77f87f513ede3
Christopher Robin Chase <christopherchase011@gmail.com>
2025-04-14 23:24:56 +0800

    chore: just rename the proxy in browsersync

--- browsersync.config.js
+++ browsersync.config.js
@@ -3,7 +3,7 @@ import browserSync from 'browser-sync'
 const bs = browserSync.create()
 
 bs.init({
-    proxy: 'http://upgaa.dev.com/', // Adjust to your local WP URL
+    proxy: 'http://localhost:8000/', // Adjust to your local WP URL
     files: ['**/*.php', 'dist/**/*.js', 'dist/**/*.css'],
     notify: false,
     open: false,
