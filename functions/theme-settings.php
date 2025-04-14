35810f1919b87163572b44320cf8cc4998401edc
Christopher Robin Chase <christopherchase011@gmail.com>
2025-04-14 23:27:16 +0800

    chore: rename functions in theme-settings

--- functions/theme-settings.php
+++ functions/theme-settings.php
@@ -1,18 +1,17 @@
 <?php
 
-function load_upgaa_theme_settings() {
+function load_custom_theme_settings() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
     add_theme_support( 'custom-logo' );
 }
 
-add_action( 'after_setup_theme', 'load_upgaa_theme_settings' );
+add_action( 'after_setup_theme', 'load_custom_theme_settings' );
 
 
-function load_upgaa_scripts() {
+function load_custom_scripts() {
     wp_enqueue_style('wp-block-library');
-    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', [], null);
-    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', [], null, true);
+    // add custom styles here and scripts here
 }
-add_action('wp_enqueue_scripts', 'load_upgaa_scripts');
+add_action('wp_enqueue_scripts', 'load_custom_scripts');
